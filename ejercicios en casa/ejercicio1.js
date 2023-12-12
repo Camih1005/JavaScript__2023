3ff6848973bc26451e2d806085daa29174fbfde9
Camilo Hernandez <145624469+Camih1005@users.noreply.github.com>
2023-12-12 14:17:38 -0500

    Add files via upload

---
+++ ejercicios en casa/ejercicio1.js
@@ -0,0 +1,43 @@
+function suma(){
+    // contenido de la funcion
+    //funciones solo pueden retornar un valor
+    //buena practica es no poner variables globales dentro de una funcion 
+    //siempre pasar por parametroo
+    //el nombre de la funcion es unico
+    return 
+}
+
+//tipoos de funciones
+//  funcion nombrada
+
+function popo() {
+
+}
+
+//funcion anonima
+
+const camilooo = function(){
+
+}
+
+//funcion flecha
+
+const camilo = () =>  {
+
+}
+
+ //callbacks
+
+//son funciones a las que se le pasa funciones como parametro
+
+function suma(a, b){
+    return a+b
+}
+
+function operacion(op,num,num2){
+
+}
+
+let nombres = []
+
+
